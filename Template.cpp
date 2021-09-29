1b0012ffb5181dfe1188335de08236736e27e5a6
avanishpython46 <73603525+avanishpython46@users.noreply.github.com>
2021-09-29 18:35:10 +0530

    Template.cpp
    This is basically my template for competitive programming

---
+++ Template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long int
+#define FOR(i,x) for (int i=0;i<n;i++);
+#define FR(x,a,b) for (int x=a;x<=b;x++)
+#define ull unsigned long long
+#define read(x) int x; cin >> x;
+#define all(x) (x.begin(),x.end())
+#define pb push_back
+#define rmp pop_back
+#define print(x) cout << x << "\n";
+#define ld long double
+
+int main() {
+	
+	return 0;
+}
